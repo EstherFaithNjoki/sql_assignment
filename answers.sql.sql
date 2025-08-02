84831a3b789e9b59faa7397f29dcbe409a8ab074
EstherFaithNjoki <estherfaithnjoki@gmail.com>
2025-08-02 02:25:08 -1200

    Add files via upload

---
+++ answers.sql.sql
@@ -0,0 +1,8 @@
+-- Assignment: Introduction to Databases
+-- Author: Esther Faith Njoki Maina
+
+-- Question 1: Create a new database
+CREATE DATABASE salesDB;
+
+-- Question 2: Drop the database called demo
+DROP DATABASE demo;
